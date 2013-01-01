49e41bbbf1156696a2aa5920ab0f1aab4d583397
Yurii Rashkovskii <yrashk@gmail.com>
2013-01-01 12:12:52 -0800

    Add expm package

---
+++ package.head.exs
@@ -0,0 +1,6 @@
+Expm.Package.new(name: "exmsgpack", description: "MessagePack for Elixir",
+                 version: :head, keywords: ["msgpack","messagepack","pack","binary","term"], 
+                 maintainers: [[name: "Yurii Rashkovskii", 
+                                email: "yrashk@gmail.com"]],
+                 repositories: [[github: "yrashk/exmsgpack"]])
+    
